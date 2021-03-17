ac832e17e0b95403f328b895d323ddf05fe70043
Carlos Moraes Jr <carlos.moraes.as@gmail.com>
2021-03-17 14:32:45 -0300

    Estrutura da API, config, criação do banco de dados.

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"api/src/config"
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.Carregar()
+	fmt.Println(config.Porta, config.StringConexao)
+
+	fmt.Println("Rodando API.")
+
+	r := router.Gerar()
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
+}
---
+++ src/controllers/usuarios.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarUsuario insere um usuário no banco de dados
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando Usuário"))
+}
+
+// BuscarUsuarios busca todos os usuários do banco de dados
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando todos os usuários"))
+}
+
+// BuscarUsuario busca um usuário no banco de dados
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando um usuário"))
+}
+
+// AtualizarUsuario atualiza um usuário do banco de dados
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Update em um usuário"))
+}
+
+// DeletarUsuario exclui um usuario do banco de dados
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Excluir um usuário"))
+}
